032744305bd333ecf5b2f79c8976bde19e980ca0
Wandersonrp <wandersonrp.49@hotmail.com>
2022-05-14 12:27:39 -0300

    Alteração no parâmetro dos métodos da classe criar conta

--- src/model/services/criarconta/CriarConta.java
+++ src/model/services/criarconta/CriarConta.java
@@ -1,21 +1,22 @@
 package model.services.criarconta;
 
-import model.entities.cliente.Cliente;
+import model.entities.cliente.ClientePessoaFisica;
+import model.entities.cliente.ClientePessoaJuridica;
 import model.entities.conta.ContaPoupanca;
 import model.entities.conta.pessoafisica.ContaCorrentePessoaFisica;
 import model.entities.conta.pessoajuridica.ContaCorrentePessoaJuridica;
 
 public class CriarConta {
 
-	public static ContaCorrentePessoaJuridica criarContaPj(Cliente cliente) {
+	public static ContaCorrentePessoaJuridica criarContaPj(ClientePessoaJuridica cliente) {
 		return new ContaCorrentePessoaJuridica(cliente);
 	}
 	
-	public static ContaCorrentePessoaFisica criarContaPf(Cliente cliente) {
+	public static ContaCorrentePessoaFisica criarContaPf(ClientePessoaFisica cliente) {
 		return new ContaCorrentePessoaFisica(cliente);
 	}
 	
-	public static ContaPoupanca criarContaPoupanca(Cliente cliente) {
+	public static ContaPoupanca criarContaPoupanca(ClientePessoaFisica cliente) {
 		return new ContaPoupanca(cliente);
 	}
 }
